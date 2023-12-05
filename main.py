9e92939a27f9aabb718feb2badba8ec7ab2970bb
demagoit <33demagoit@gmail.com>
2023-12-05 21:25:10 +0200

    Revert "releise 1.0.0"
    This reverts commit 56ee50287ca8412dd0213104b8f88139654890fb.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+'''
+Наш асистент вже вміє взаємодіяти з користувачем за допомогою командного рядка, 
+отримуючи команди та аргументи та виконуючи потрібні дії. 
+У цьому завданні треба буде попрацювати над внутрішньою логікою асистента, 
+над тим, як зберігаються дані, які саме дані і що з ними можна зробити.
+'''
 from collections import UserDict
 
 # {name:{phone:[], email:[], favorite: False}}
 
+
 class Field:
+    '''
+    Базовий клас для полів запису. Буде батьківським для всіх полів, 
+    у ньому реалізується логіка загальна для всіх полів
+    '''
+
     def __init__(self, value):
         self.value = value
 
@@ -11,68 +23,71 @@ class Field:
 
 
 class Name(Field):
-    # self.__str__()
-    pass    
+    '''
+    Клас для зберігання імені контакту. Обов'язкове поле.
+    '''
+
+    def __init__(self, value):
+        if not value.isidentifier():
+            raise ValueError
+        if (len(value) == 0) | (value is None):
+            raise ValueError
+        super().__init__(value)
 
 
 class Phone(Field):
+    '''
+    Клас для зберігання номера телефону. Має валідацію формату (10 цифр). 
+    Необов'язкове поле з телефоном та таких один запис Record може містити декілька.
+    '''
+
     def __init__(self, value):
-        
-        # if value.startswith('+'):
-        #     value = value[1:]
+
         if not value.isdigit():
             raise ValueError
-        # if len(value) == 12:
-        #     value = '+' + value
-        # elif len(value) == 10:
-        #     value = '+38' + value
-        # elif len(value) == 9:
-        #     value = '+380' + value
-        # else:
-        #     raise ValueError
-
         if len(value) != 10:
             raise ValueError
-        
+
         super().__init__(value)
-        
 
 
 class Record():
-    
+    '''
+    Клас для зберігання інформації про контакт, включаючи ім'я та список телефонів. 
+    Відповідає за логіку додавання/видалення/редагування необов'язкових полів та 
+    зберігання обов'язкового поля Name
+    '''
+
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
     def add_phone(self, phone):
+        '''додавання об'єктів'''
         self.phones.append(Phone(phone))
-    
 
     def remove_phone(self, phone):
-        # phone = Phone(phone).value
+        '''видалення об'єктів'''
         for item in self.phones:
             if item.value == phone:
                 self.phones.remove(item)
                 return
         raise ValueError
-        
+
     def edit_phone(self, old_phone, new_phone):
-        # print(old_phone)
-        # print([x.value for x in self.phones])
+        '''редагування об'єктів'''
         old_phone = Phone(old_phone)
         new_phone = Phone(new_phone)
 
-        
         for item in self.phones:
             if item.value == old_phone.value:
                 self.phones.insert(self.phones.index(item), new_phone)
                 self.phones.remove(item)
                 return
         raise ValueError
-        
+
     def find_phone(self, phone):
-        # print(phone, type(phone))
-        # print(self.phones)
+        '''пошук об'єктів'''
         for item in self.phones:
             if item.value == phone:
                 return item
@@ -81,61 +96,27 @@ class Record():
     def __str__(self):
         return f'Contact name: {self.name}, phones: {", ".join([x.value for x in self.phones])}'
 
-    # def add_phone(self, phone):
-    #     self.phones.append(Phone(phone).__str__())
-
-    # def remove_phone(self, phone):
-    #     phone = Phone(phone).__str__()
-    #     if phone in self.phones:
-    #         self.phones.remove(phone)
-    #     else:
-    #         pass
-    #         # raise
-
-    # def edit_phone(self, old_phone, new_phone):
-    #     old_phone = Phone(old_phone).__str__()
-    #     new_phone = Phone(new_phone).__str__()
-
-    #     if old_phone in self.phones:
-    #         self.phones.insert(self.phones.index(old_phone), new_phone)
-    #         self.remove_phone(old_phone)
-    #     else:
-    #         # raise
-    #         pass
-
-    # def find_phone(self, phone):
-    #     print(phone, type(phone))
-    #     print(self.phones)
-    #     for item in self.phones:
-    #         if phone in item:
-    #             return item
-    #     return None
-
-    # def __str__(self):
-    #     return f'Contact name: {self.name}, phones: {", ".join(self.phones)}'
-
 
 class AddressBook(UserDict):
-    # def __init__(self):
-    #     pass
+    '''
+    Клас для зберігання та управління записами. 
+    Успадковується від UserDict, та містить логіку пошуку за записами до цього класу
+    '''
 
     def add_record(self, contact):
-        # додає запис до self.data.
-        self.data[contact.name.__str__()] = contact
+        '''додає запис до self.data.'''
+        self.data[str(contact.name)] = contact
 
     def find(self, name):
-        # знаходить запис за ім'ям.
+        '''знаходить запис за ім'ям.'''
         if name in self.data.keys():
-            return self.data.get(name) 
-        # else:
-        #     raise KeyError
+            return self.data.get(name)
+        return None
 
     def delete(self, name):
-        # який видаляє запис за ім'ям.
+        '''який видаляє запис за ім'ям.'''
         if name in self.data.keys():
             self.data.pop(name)
-        # else:
-        #     raise KeyError
 
 
 # book = AddressBook()
